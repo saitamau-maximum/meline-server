06062bb9f89ed6947b0e5a557178f8611470000b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-12 08:30:32 +0900

    feat: adapt oauth system to cors

--- usecase/auth_interactor_test.go
+++ usecase/auth_interactor_test.go
@@ -11,12 +11,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAuthInteractor_CreateAccessToken(t *testing.T) {
+func TestAuthInteractor_GenerateJWT(t *testing.T) {
 	interactor := usecase.NewAuthInteractor()
 
 	var userId uint64 = 1
 
-	result, err := interactor.CreateAccessToken(context.Background(), userId)
+	result, err := interactor.GenerateJWTWithUserID(context.Background(), userId)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, result)
 }
@@ -34,19 +34,6 @@ func TestAuthInteractor_GenerateStateCookie(t *testing.T) {
 	cookie := interactor.GenerateStateCookie("this_is_state", false)
 	assert.Equal(t, "state", cookie.Name)
 	assert.Equal(t, "this_is_state", cookie.Value)
-	assert.Equal(t, "/", cookie.Path)
-	assert.True(t, cookie.HttpOnly)
-	assert.Equal(t, http.SameSiteLaxMode, cookie.SameSite)
-	assert.True(t, cookie.Secure)
-	assert.True(t, cookie.Expires.After(time.Now()))
-}
-
-func TestAuthInteractor_GenerateAccessTokenCookie(t *testing.T) {
-	interactor := usecase.NewAuthInteractor()
-
-	cookie := interactor.GenerateAccessTokenCookie("token", false)
-	assert.Equal(t, "access_token", cookie.Name)
-	assert.Equal(t, "token", cookie.Value)
 	assert.Equal(t, "/", cookie.Path)
 	assert.True(t, cookie.HttpOnly)
 	assert.Equal(t, http.SameSiteLaxMode, cookie.SameSite)
