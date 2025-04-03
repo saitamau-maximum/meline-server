e089c9bcdea45bc0911023d476b09ef4999020a3
Daaaai0809 <dai.tsuruga@80and.co>
2025-04-03 11:04:35 +0900

    feat : webpush送信ロジックの実装

--- usecase/webpush_interactor.go
+++ usecase/webpush_interactor.go
@@ -3,7 +3,9 @@ package usecase
 import (
 	"context"
 	"fmt"
+	"strconv"
 
+	"golang.org/x/sync/errgroup"
 	"github.com/SherClockHolmes/webpush-go"
 	"github.com/saitamau-maximum/meline/config"
 	"github.com/saitamau-maximum/meline/domain/repository"
@@ -14,18 +16,20 @@ import (
 type IWebPushInteractor interface {
 	StoreSubscription(ctx context.Context, userID string, subscription *webpush.Subscription) error
 	DeleteSubscription(ctx context.Context, key string) error
-	SendWebPushNotification(ctx context.Context, message []byte, subscription *webpush.Subscription) error
+	SendWebPushNotification(ctx context.Context, channelID uint64, message []byte) error
 	GetPublicKey() *response.GetPublicKeyResponse
 }
 
 type WebPushInteractor struct {
+	channelRepository 	repository.IChannelRepository
 	webPushRepository     repository.IWebPushRepository
 	pushServiceRepository repository.IPushServiceRepository
 	webPushPresenter      presenter.IWebPushPresenter
 }
 
-func NewWebPushInteractor(webPushRepository repository.IWebPushRepository, pushServiceRepository repository.IPushServiceRepository, webPushPresenter presenter.IWebPushPresenter) IWebPushInteractor {
+func NewWebPushInteractor(channelRepository repository.IChannelRepository, webPushRepository repository.IWebPushRepository, pushServiceRepository repository.IPushServiceRepository, webPushPresenter presenter.IWebPushPresenter) IWebPushInteractor {
 	return &WebPushInteractor{
+		channelRepository: 	channelRepository,
 		webPushRepository:     webPushRepository,
 		pushServiceRepository: pushServiceRepository,
 		webPushPresenter:      webPushPresenter,
@@ -33,11 +37,7 @@ func NewWebPushInteractor(webPushRepository repository.IWebPushRepository, pushS
 }
 
 func (i *WebPushInteractor) StoreSubscription(ctx context.Context, userID string, subscription *webpush.Subscription) error {
-	subscriptionData := map[string]*webpush.Subscription{
-		config.SUBSCRIPTION_FIELD: subscription,
-	}
-
-	if i.webPushRepository.SetSubscription(ctx, userID, subscriptionData) != nil {
+	if i.webPushRepository.SetSubscription(ctx, userID, subscription) != nil {
 		return fmt.Errorf("[ERROR] : failed to store subscription")
 	}
 
@@ -51,10 +51,46 @@ func (i *WebPushInteractor) DeleteSubscription(ctx context.Context, key string) 
 	return nil
 }
 
-func (i *WebPushInteractor) SendWebPushNotification(ctx context.Context, message []byte, subscription *webpush.Subscription) error {
-	if err := i.pushServiceRepository.SendWebPushNotification(ctx, message, subscription); err != nil {
-		return err
+func (i *WebPushInteractor) SendWebPushNotification(ctx context.Context, channelID uint64, message []byte) error {
+	channel, err := i.channelRepository.FindByID(ctx, channelID)
+	if err != nil {
+		return fmt.Errorf("[ERROR] : failed to find channel by ID: %v", err)
 	}
+
+	users := channel.Users
+	if len(users) == 0 {
+		return nil
+	}
+
+	subscriptions := make([]*webpush.Subscription, 0)
+	userIDs := make([]string, 0)
+	for _, user := range users {
+		userIDs = append(userIDs, strconv.FormatUint(user.ID, 10))
+	}
+	
+	subscriptions, err = i.webPushRepository.GetSubscriptions(ctx, userIDs)
+	if err != nil {
+		return fmt.Errorf("[ERROR] : failed to get subscriptions: %v", err)
+	}
+	if len(subscriptions) == 0 {
+		return nil
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, subscription := range subscriptions {
+		eg.Go(func() error {
+			if err := i.pushServiceRepository.SendWebPushNotification(ctx, message, subscription); err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return fmt.Errorf("[ERROR] : failed to send web push notification: %v", err)
+	}
+
 	return nil
 }
 
