11f0f8376cff9cb88885639f2c442a30e7aea494
Daaaai0809 <dai.tsuruga@80and.co>
2025-04-07 01:02:14 +0900

    fix : redisコンテナ消し, env周りの修正 #12

--- config/env.go
+++ config/env.go
@@ -18,10 +18,6 @@ const (
 	_CLIENT_ORIGIN           = "CLIENT_ORIGIN"
 	_JWT_SECRET              = "JWT_SECRET"
 	_OAUTH_OTT_EXPIRE        = "OAUTH_OTT_EXPIRE"
-	_REDIS_HOST              = "REDIS_HOST"
-	_REDIS_PORT              = "REDIS_PORT"
-	_REDIS_PASSWORD          = "REDIS_PASSWORD"
-	_REDIS_DB                = "REDIS_DB"
 	_VAPID_PUBLIC_KEY        = "VAPID_PUBLIC_KEY"
 	_VAPID_PRIVATE_KEY       = "VAPID_PRIVATE_KEY"
 )
@@ -38,10 +34,6 @@ var (
 	CLIENT_ORIGIN           = os.Getenv(_CLIENT_ORIGIN)
 	JWT_SECRET              = os.Getenv(_JWT_SECRET)
 	OAUTH_OTT_EXPIRE        = os.Getenv(_OAUTH_OTT_EXPIRE)
-	REDIS_HOST              = os.Getenv(_REDIS_HOST)
-	REDIS_PORT              = os.Getenv(_REDIS_PORT)
-	REDIS_PASSWORD          = os.Getenv(_REDIS_PASSWORD)
-	REDIS_DB                = os.Getenv(_REDIS_DB)
 	VAPID_PUBLIC_KEY        = os.Getenv(_VAPID_PUBLIC_KEY)
 	VAPID_PRIVATE_KEY       = os.Getenv(_VAPID_PRIVATE_KEY)
 
@@ -67,12 +59,6 @@ var (
 		_VAPID_PUBLIC_KEY,
 		_VAPID_PRIVATE_KEY,
 	}
-	requiredRedisEnv = []string{
-		_REDIS_HOST,
-		_REDIS_PORT,
-		_REDIS_PASSWORD,
-		_REDIS_DB,
-	}
 )
 
 func ValidateDBEnv() error {
@@ -87,16 +73,6 @@ func ValidateDBEnv() error {
 
 func ValidateAppEnv() error {
 	for _, env := range requiredAppEnv {
-		if os.Getenv(env) == "" {
-			return fmt.Errorf("%s is not set", env)
-		}
-	}
-
-	return nil
-}
-
-func ValidateRedisEnv() error {
-	for _, env := range requiredRedisEnv {
 		if os.Getenv(env) == "" {
 			return fmt.Errorf("%s is not set", env)
 		}
